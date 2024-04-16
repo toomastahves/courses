5a637fcc3678d884f576bd073bdeeba1f1a32a8d
Toomas Tahves <toomastahves@hotmail.com>
2024-04-16 18:33:01 +0300

    add user controller

---
+++ courses-backend/app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return UserResource::collection(User::all());
+    }
+
+}
--- courses-backend/routes/api.php
+++ courses-backend/routes/api.php
@@ -2,7 +2,9 @@
 
 use App\Http\Controllers\Api\CoordinatorController;
 use App\Http\Controllers\Api\CourseController;
+use App\Http\Controllers\Api\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('courses', CourseController::class);
 Route::apiResource('courses.coordinators', CoordinatorController::class)->scoped()->except(['update']);
+Route::apiResource('users', UserController::class)->scoped()->only('index');
